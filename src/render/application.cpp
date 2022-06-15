b62782f23157d3730689568e714540a543fc3c6c
tencentholdings <vanctardi@protonmail.com>
2022-06-15 16:50:57 -0400

    3D cube test

--- src/render/application.cpp
+++ src/render/application.cpp
@@ -34,20 +34,69 @@ namespace scandium {
 		vkDeviceWaitIdle(engineDevice.device());
 	}
 
+	std::unique_ptr<ScandiumModel> createCubeModel(EngineDevice& device, glm::vec3 offset) {
+	std::vector<ScandiumModel::Vertex> vertices{
+		// left face (white)
+		{{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+		{{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+		{{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+		{{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+		{{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+		{{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+	
+		// right face (yellow)
+		{{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+		{{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+		{{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+		{{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+		{{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+		{{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+	
+		// top face (orange, remember y axis points down)
+		{{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+		{{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+		{{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+		{{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+		{{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+		{{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+	
+		// bottom face (red)
+		{{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+		{{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+		{{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+		{{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+		{{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+		{{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+	
+		// nose face (blue)
+		{{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+		{{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+		{{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+		{{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+		{{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+		{{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+	
+		// tail face (green)
+		{{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+		{{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+		{{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+		{{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+		{{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+		{{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+	
+	};
+	for (auto& v : vertices) {
+		v.position += offset;
+	}
+	return std::make_unique<ScandiumModel>(device, vertices);
+	}
+
 	void Application::loadGameObjects(){
-		std::vector<ScandiumModel::Vertex> vertices {
-			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}}, 
-			{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}}, 
-			{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
-		};
-		auto scandiumModel = std::make_shared<ScandiumModel>(engineDevice, vertices);
-		auto triangle = GameObject::createGameObject();
-		triangle.model = scandiumModel;
-		triangle.color = {0.1f, 0.8f, 0.1f};
-		triangle.Transform2D.translate(0.0f, 0.0f);
-		triangle.Transform2D.scale(1.0f, 0.5f);
-		triangle.Transform2D.rotate(0.0f, 0.0f, 0.0f);
-		triangle.Transform2D.flip_x();
-		gameObjects.push_back(std::move(triangle));
+		std::shared_ptr<ScandiumModel> scandiumModel = createCubeModel(engineDevice, {.0f, .0f, .0f});
+		auto cube = GameObject::createGameObject();	
+		cube.model = scandiumModel;
+		cube.Transform2D.translate(0.0f, 0.0f, 0.5f);
+		cube.Transform2D.scale(0.5f, 0.5f, 0.5f);
+		gameObjects.push_back(std::move(cube));
 	}
 }
