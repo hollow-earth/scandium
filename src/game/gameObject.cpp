914ae9877986a4ff498829c2fba051f7bf53d6d6
tencentholdings <vanctardi@protonmail.com>
2022-06-15 16:50:46 -0400

    Improved memory usage, made 3D changes

--- src/game/gameObject.cpp
+++ src/game/gameObject.cpp
@@ -2,16 +2,17 @@
 #include <iostream>
 
 namespace scandium {
-	void Transform2DComponent::scale(float x_scaling, float y_scaling){
-		scaling = {x_scaling, y_scaling, 1.0f};
+	void Transform2DComponent::scale(const float x_scaling, const float y_scaling, const float z_scaling){
+		scaling = {x_scaling, y_scaling, z_scaling};
 	}
 
-	void Transform2DComponent::scale(float scaling_factor){
-		scaling = {scaling_factor, scaling_factor, 1.0f};
+	void Transform2DComponent::scale(const float scaling_factor){
+		scaling = {scaling_factor, scaling_factor, scaling_factor};
 	}
-
-	void Transform2DComponent::translate(float x_translation, float y_translation){
-		position = {position.x + x_translation, position.y + y_translation, 1.0f};
+	// x = [-1, 1], y = [-1, 1], z = [0, 1]
+	//TODO implement this?
+	void Transform2DComponent::translate(const float x_translation, const float y_translation, const float z_translation){
+		position = {position.x + x_translation, position.y + y_translation, position.z + z_translation};
 	}
 
 	void Transform2DComponent::rotate(const float theta_x, 
@@ -22,24 +23,22 @@ namespace scandium {
 	// Flip about the x-axis
 	void Transform2DComponent::flip_x(){
 		rotation.x += glm::pi<float>();
-		if (rotation.x >= glm::two_pi<float>()){
-			rotation.x -= glm::two_pi<float>();
-		}
+		//if (rotation.x >= glm::two_pi<float>()){
+		//	rotation.x -= glm::two_pi<float>();
+		//}
 	}
 	
 	// Flip about the y-axis
 	void Transform2DComponent::flip_y(){
 		rotation.y += glm::pi<float>();
-		if (rotation.y >= glm::two_pi<float>()){
-			rotation.y -= glm::two_pi<float>();
-		}
+		//if (rotation.y >= glm::two_pi<float>()){
+		//	rotation.y -= glm::two_pi<float>();
+		//}
 	}
 
 	glm::mat4x4 Transform2DComponent::transform(){
 		glm::mat4x4 transform = glm::translate(glm::mat4x4(1.0f), position);
-		transform = glm::rotate(transform, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-		transform = glm::rotate(transform, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
-		transform = glm::rotate(transform, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		transform = transform * glm::eulerAngleYXZ(rotation.y, rotation.x, rotation.z);
 		transform = glm::scale(transform, scaling);
 		return transform;
 	}
--- src/game/gameObject.hpp
+++ src/game/gameObject.hpp
@@ -6,24 +6,26 @@
 #include <glm/vec3.hpp>
 #include <glm/matrix.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
 #include <memory>
 
 namespace scandium
 {	
 	struct Transform2DComponent{
-		glm::vec3 position = {0.0f, 0.0f, 0.0f};
-		glm::vec3 scaling = {1.0f, 1.0f, 1.0f};
-		glm::vec3 rotation = {0.0f, 0.0f, 1.0f};
-
 		void rotate(const float theta_x, const float theta_y, const float theta_z);
 		void rotate(const float theta_z);
 		void flip_x();
 		void flip_y();
-		void scale(float x_scaling, float y_scaling);
-		void scale(float scaling);
+		void scale(const float x_scaling, const float y_scaling, const float z_scaling);
+		void scale(const float scaling);
 		//void scale(glm::mat2x2 scaleMat);
-		void translate(float x_translation, float y_translation);
+		void translate(const float x_translation, const float y_translation, const float z_translation);
 		glm::mat4x4 transform();
+
+		public:
+			glm::vec3 position = {0.0f, 0.0f, 0.0f};
+			glm::vec3 scaling = {1.0f, 1.0f, 1.0f};
+			glm::vec3 rotation = {0.0f, 0.0f, 1.0f};
 	};
 
 	class GameObject{
